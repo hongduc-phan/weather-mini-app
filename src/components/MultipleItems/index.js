e854b1ac948ffc1c2665267c4b167ffcc21e816f
Hong Duc <phduc.phanhongduc@gmail.com>
2019-10-20 15:22:18 +0700

    init arrow slick

--- src/components/MultipleItems/index.js
+++ src/components/MultipleItems/index.js
@@ -10,7 +10,8 @@ export default class MultipleItems extends Component {
       infinite: true,
       speed: 500,
       slidesToShow: 3,
-      slidesToScroll: 3
+      slidesToScroll: 3,
+      accessibility: true
     };
     return (
       <div>
